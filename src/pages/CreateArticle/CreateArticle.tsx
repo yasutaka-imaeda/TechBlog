e3d4d353938c6df3ec8ce390da8a07408e8ea684
yasutaka.imaeda <yasu89baka1013@icloud.com>
2022-01-18 19:23:10 +0900

    react-simplemde-editorライブラリのインストール

--- src/pages/CreateArticle/CreateArticle.tsx
+++ src/pages/CreateArticle/CreateArticle.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../component/Header/Header";
 import styles from "./CreateArticle.module.scss";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const CreateArticle: React.FC = () => {
+  const [markdown, setMarkdown] = useState("");
+
   return (
     <div className={styles.root}>
       <div className={styles.container}>
@@ -10,6 +14,19 @@ const CreateArticle: React.FC = () => {
           <Header />
         </div>
         <div>記事編集</div>
+        <div className={styles.titleWrapper}>
+          <div>タイトル</div>
+          <input />
+        </div>
+        <div className={styles.inputWrapper}>
+          <div>入力欄</div>
+          <SimpleMDE onChange={(e) => setMarkdown(e)} />
+        </div>
+        <div className={styles.preview}>
+          <div>プレビュー</div>
+          <div>入力内容表示</div>
+        </div>
+        <div className={styles.sendBtn}>投稿する</div>
       </div>
     </div>
   );
